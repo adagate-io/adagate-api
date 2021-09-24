cfb9e9afadf76de3c37ec4752b8a783db7fc7261
William Wolff <william@cityxcape.com>
2021-09-24 13:17:12 +0200

    [TESTS] Added abstract testing class

---
+++ src/test/java/io/adagate/AbstractVerticleTest.java
@@ -0,0 +1,10 @@
+package io.adagate;
+
+import io.adagate.handlers.database.AbstractDatabaseHandler;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public abstract class AbstractVerticleTest {
+    protected final static Logger LOGGER = LoggerFactory.getLogger(AbstractVerticleTest.class);
+
+}
