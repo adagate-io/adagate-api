ea9871aaaad1e3a7a0572002a3c1e75ed07c5ba9
William Wolff <william@cityxcape.com>
2021-09-28 15:51:43 +0200

    [TESTS] Added constants for address testing

---
+++ src/test/java/io/adagate/handlers/routes/addresses/AddressTestContstants.java
@@ -0,0 +1,6 @@
+package io.adagate.handlers.routes.addresses;
+
+final class AddressTestContstants {
+
+    static final String TEST_ADDRESS = "addr1qyl5hqy7p3yvredqdpvx9z03803m4vqr86ls4mc8fhw60zxpkyw8gfsyt5gmz3sqthavuyjx2f0nd34vawdcr437v4rsl5r5wt";
+}
