d28419dc60a9ca3ccafbc0351ba566190d9aca1f
William Wolff <william@cityxcape.com>
2021-10-13 21:05:04 +0200

    [API] Updated Router to include route names and added missing todos for endpoints

--- src/main/java/io/adagate/verticles/webserver/ApiRouter.java
+++ src/main/java/io/adagate/verticles/webserver/ApiRouter.java
@@ -1,5 +1,6 @@
 package io.adagate.verticles.webserver;
 
+import io.adagate.CardanoApiModule;
 import io.adagate.handlers.routes.FailureHandler;
 import io.adagate.handlers.routes.accounts.*;
 import io.adagate.handlers.routes.addresses.GetAddress;
@@ -13,9 +14,11 @@ import io.adagate.handlers.routes.epochs.GetEpochById;
 import io.adagate.handlers.routes.epochs.GetLatestEpoch;
 import io.adagate.handlers.routes.genesis.GetGenesis;
 import io.adagate.handlers.routes.pools.GetPoolByIdOrHash;
-import io.adagate.handlers.security.ApiAccessHandler;
+import io.adagate.handlers.routes.pools.GetPoolMetadata;
 import io.adagate.verticles.database.DatabaseEventbusAddress;
 import io.vertx.core.Vertx;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.bridge.PermittedOptions;
 import io.vertx.ext.web.handler.LoggerFormat;
@@ -25,6 +28,8 @@ import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.ext.web.impl.RouterImpl;
 
 public final class ApiRouter extends RouterImpl {
+    final static Logger LOGGER = LoggerFactory.getLogger(CardanoApiModule.class);
+
     private final Vertx vertx;
 
     public ApiRouter(Vertx vertx, JsonObject config) {
@@ -53,106 +58,128 @@ public final class ApiRouter extends RouterImpl {
 //                    .allowCredentials(false)
 //                    .addOrigin(config.getString("origin", "*"))
 //            )
-            .handler(new ApiAccessHandler(vertx, config))
+//            .handler(new ApiAccessHandler(vertx, config))
             .failureHandler(new FailureHandler(vertx));
 
         /* Accounts */
-        get("/accounts/:stakeAddress").handler(new GetAccountByStakeAddress(vertx)); // TODO: Add controlled amount
-        get("/accounts/:stakeAddress/rewards").handler(new GetAccountRewards(vertx));
-        get("/accounts/:stakeAddress/history").handler(new GetAccountHistory(vertx)); // TODO: Add amount column
-        get("/accounts/:stakeAddress/delegations").handler(new GetAccountDelegations(vertx)); // TODO: Add amount column
-        get("/accounts/:stakeAddress/withdrawals").handler(new GetAccountWithdrawals(vertx));
-//        get("/accounts/:stakeAddress/mirs").handler();
-        get("/accounts/:stakeAddress/addresses").handler(new GetAccountAddresses(vertx));
-//        get("/accounts/:stakeAddress/addresses/assets").handler();
+        get("/accounts/:stakeAddress").handler(new GetAccountByStakeAddress(vertx))
+                .setName("GET Account by Stake Address");
+        get("/accounts/:stakeAddress/rewards").handler(new GetAccountRewards(vertx))
+                .setName("GET Account Rewards");
+        get("/accounts/:stakeAddress/history").handler(new GetAccountHistory(vertx))
+                .setName("GET Account History");
+        get("/accounts/:stakeAddress/delegations").handler(new GetAccountDelegations(vertx))
+                .setName("GET Account Delegations"); // TODO: Add amount column
+        get("/accounts/:stakeAddress/withdrawals").handler(new GetAccountWithdrawals(vertx))
+                .setName("GET Account Withdrawals");
+// TODO: get("/accounts/:stakeAddress/mirs").handler();
+        get("/accounts/:stakeAddress/addresses").handler(new GetAccountAddresses(vertx))
+                .setName("GET Account Addresses");
+// TODO: get("/accounts/:stakeAddress/addresses/assets").handler();
 
         /* Addresses */
-        get("/addresses/:address").handler(new GetAddress(vertx));
-        get("/addresses/:address/total").handler(new GetAddressTotal(vertx));
-        get("/addresses/:address/utxos").handler(new GetAddressUTXOs(vertx));
-        get("/addresses/:address/transactions").handler(new GetAddressTransactions(vertx));
+        get("/addresses/:address").handler(new GetAddress(vertx))
+                .setName("GET Address Information");
+        get("/addresses/:address/total").handler(new GetAddressTotal(vertx))
+                .setName("GET Address Total");
+        get("/addresses/:address/utxos").handler(new GetAddressUTXOs(vertx))
+                .setName("GET Address UTxOs");
+        get("/addresses/:address/transactions").handler(new GetAddressTransactions(vertx))
+                .setName("GET Address Transactions");
 
         /* Assets */
-        get("/assets").handler(new GetAssets(vertx));
-        get("/assets/:assetId").handler(new GetAssetById(vertx));
-//        get("/assets/:assetId/history").handler();
-//        get("/assets/:assetId/transactions").handler();
-//        get("/assets/:assetId/addresses").handler();
-//        get("/assets/policy/:policyId").handler();
+        get("/assets").handler(new GetAssets(vertx))
+                .setName("GET Asset Information");
+        get("/assets/:assetId").handler(new GetAssetById(vertx))
+                .setName("GET Specific Asset by id");
+// TODO: get("/assets/:assetId/history").handler();
+// TODO: get("/assets/:assetId/transactions").handler();
+// TODO: get("/assets/:assetId/addresses").handler();
+// TODO: get("/assets/policy/:policyId").handler();
 
         /* Blocks */
-        get("/blocks/latest").handler(new GetLatestBlock(vertx));
+        get("/blocks/latest").handler(new GetLatestBlock(vertx))
+                .setName("GET latest Block");
         get("/blocks/latest/transactions")
             .handler(new GetLatestBlockTransactions(vertx))
-            .handler(new GetBlockTransactions(vertx));
-        get("/blocks/:id").handler(new GetBlockByNumberOrHash(vertx));
-        get("/blocks/slot/:slotNumber").handler(new GetBlockBySlotNumber(vertx));
-        get("/blocks/epoch/:epochNumber/slot/:slotNumber").handler(new GetBlockByEpochSlotNumber(vertx));
-        get("/blocks/:id/next").handler(new GetNextBlock(vertx));
-        get("/blocks/:id/previous").handler(new GetPreviousBlock(vertx));
-        get("/blocks/:id/transactions").handler(new GetBlockTransactions(vertx));
+            .handler(new GetBlockTransactions(vertx))
+            .setName("GET latest Block's Transactions");
+        get("/blocks/:id").handler(new GetBlockByNumberOrHash(vertx))
+                .setName("GET Block by id");
+        get("/blocks/slot/:slotNumber").handler(new GetBlockBySlotNumber(vertx))
+                .setName("GET Block by Slot Number");
+        get("/blocks/epoch/:epochNumber/slot/:slotNumber").handler(new GetBlockByEpochSlotNumber(vertx))
+                .setName("GET Block by Epoch and Epoch Slot Number");
+        get("/blocks/:id/next").handler(new GetNextBlock(vertx))
+                .setName("GET next Block");
+        get("/blocks/:id/previous").handler(new GetPreviousBlock(vertx))
+                .setName("Get previous Block");
+        get("/blocks/:id/transactions").handler(new GetBlockTransactions(vertx))
+                .setName("Get Block Transactions");
 
         /* Epochs */
-        get("/epochs/latest").handler(new GetLatestEpoch(vertx));
-//        get("/epochs/latest/parameters").handler();
-        get("/epochs/:epochNumber").handler(new GetEpochById(vertx));
-//        get("/epochs/:epochNumber/next").handler(new GetEpochById(vertx, e -> e + 1));
-//        get("/epochs/:epochNumber/previous").handler(new GetEpochById(vertx, e -> e - 1));
-//        get("/epochs/:epochNumber/stakes").handler();
-//        get("/epochs/:epochNumber/stakes/:poolId").handler();
-//        get("/epochs/:epochNumber/blocks").handler();
-//        get("/epochs/:epochNumber/blocks/:poolId").handler();
-//        get("/epochs/:epochNumber/parameters").handler();
+        get("/epochs/latest").handler(new GetLatestEpoch(vertx))
+                .setName("GET latest Epoch");
+// TODO: get("/epochs/latest/parameters").handler();
+        get("/epochs/:epochNumber").handler(new GetEpochById(vertx))
+                .setName("GET Epoch by id");
+// TODO: get("/epochs/:epochNumber/next").handler(new GetEpochById(vertx, e -> e + 1));
+// TODO: get("/epochs/:epochNumber/previous").handler(new GetEpochById(vertx, e -> e - 1));
+// TODO: get("/epochs/:epochNumber/stakes").handler();
+// TODO: get("/epochs/:epochNumber/stakes/:poolId").handler();
+// TODO: get("/epochs/:epochNumber/blocks").handler();
+// TODO: get("/epochs/:epochNumber/blocks/:poolId").handler();
+// TODO: get("/epochs/:epochNumber/parameters").handler();
 
         /* Ledger */
-        get("/genesis").handler(new GetGenesis(config.getString("network", "mainnet"), vertx));
-//        get("/metadata/transactions/labels").handler();
-//        get("/metadata/transactions/labels/:label").handler();
+        get("/genesis").handler(new GetGenesis(config.getString("network", "mainnet"), vertx))
+                .setName("GET Genesis of network");
+// TODO: get("/metadata/transactions/labels").handler();
+// TODO: get("/metadata/transactions/labels/:label").handler();
 
         /* Network */
-//        get("/network").handler();
+// TODO: get("/network").handler();
 
         /* Pools */
-//        get("/pools").handler();
-//        get("/pools/retired").handler();
-//        get("/pools/retiring").handler();
-        get("/pools/:poolId").handler(new GetPoolByIdOrHash(vertx)); // TODO: Periodically update total_live_stake
-//        get("/pools/:poolId/history").handler();
-//        get("/pools/:poolId/metadata").handler();
-//        get("/pools/:poolId/relays").handler();
-//        get("/pools/:poolId/delegators").handler();
-//        get("/pools/:poolId/blocks").handler();
-//        get("/pools/:poolId/updates").handler();
+// TODO: get("/pools").handler();
+// TODO: get("/pools/retired").handler();
+// TODO: get("/pools/retiring").handler();
+        get("/pools/:poolId").handler(new GetPoolByIdOrHash(vertx))
+                .setName("GET Pool by id"); // TODO: Periodically update total_live_stake
+// TODO: get("/pools/:poolId/history").handler();
+        get("/pools/:poolId/metadata").handler(new GetPoolMetadata(vertx))
+                .setName("GET Pool Metadata");
+// TODO: get("/pools/:poolId/relays").handler();
+// TODO: get("/pools/:poolId/delegators").handler();
+// TODO: get("/pools/:poolId/blocks").handler();
+// TODO: get("/pools/:poolId/updates").handler();
 
         /* Transactions */
-//        get("/transactions/:hash").handler();
-//        get("/transactions/:hash/utxos").handler();
-//        get("/transactions/:hash/stakes").handler();
-//        get("/transactions/:hash/delegations").handler();
-//        get("/transactions/:hash/withdrawals").handler();
-//        get("/transactions/:hash/mirs").handler();
-//        get("/transactions/:hash/pool_updates").handler();
-//        get("/transactions/:hash/pool_retires").handler();
-//        get("/transactions/:hash/metadata").handler();
-//        get("/transactions/:hash/metadata/cbor").handler();
+// TODO: get("/transactions/:hash").handler();
+// TODO: get("/transactions/:hash/utxos").handler();
+// TODO: get("/transactions/:hash/stakes").handler();
+// TODO: get("/transactions/:hash/delegations").handler();
+// TODO: get("/transactions/:hash/withdrawals").handler();
+// TODO: get("/transactions/:hash/mirs").handler();
+// TODO: get("/transactions/:hash/pool_updates").handler();
+// TODO: get("/transactions/:hash/pool_retires").handler();
+// TODO: get("/transactions/:hash/metadata").handler();
+// TODO: get("/transactions/:hash/metadata/cbor").handler();
 //        post("/transactions/submit").handler();
 
         /* IPFS */
 //        post("/ipfs/add").handler();
-//        get("/ipfs/gateway/:ipfsPath").handler();
+// TODO: get("/ipfs/gateway/:ipfsPath").handler();
 //        post("/ipfs/pin/add/:ipfsPath").handler();
-//        get("/ipfs/pin/list").handler();
-//        get("/ipfs/pin/list/:ipfsPath").handler();
+// TODO: get("/ipfs/pin/list").handler();
+// TODO: get("/ipfs/pin/list/:ipfsPath").handler();
 //        delete("/ipfs/pin/remove/:ipfsPath").handler();
 
         /* Nut.link*/
-//        get("/nutlink/:address").handler();
-//        get("/nutlink/:address/tickers").handler();
-//        get("/nutlink/:address/tickers/:ticker").handler();
-//        get("/nutlink/tickers/:ticker").handler();
-
-        // TODO: Remove template
-        get("/blocks").handler(ctx -> ctx.response().sendFile("public/blocks.html"));
+// TODO: get("/nutlink/:address").handler();
+// TODO: get("/nutlink/:address/tickers").handler();
+// TODO: get("/nutlink/:address/tickers/:ticker").handler();
+// TODO: get("/nutlink/tickers/:ticker").handler();
     }
 
     private void setupWebsocketBridge() {
