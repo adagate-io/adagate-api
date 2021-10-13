98ed813c1ee187ae6b9fe516629bd2addbb75e33
William Wolff <william@cityxcape.com>
2021-10-13 12:29:03 +0200

    [DB] Clean up and simplification of database subscriptions

--- src/main/java/io/adagate/verticles/database/DatabaseSubscriberVerticle.java
+++ src/main/java/io/adagate/verticles/database/DatabaseSubscriberVerticle.java
@@ -1,21 +1,36 @@
 package io.adagate.verticles.database;
 
-import io.adagate.functions.database.triggers.BlockInsertedTrigger;
-import io.adagate.handlers.database.blocks.GetBlockByNumberOrHash;
+import io.adagate.database.subscribers.AssetInsertSubscriber;
+import io.adagate.database.subscribers.BlockInsertSubscriber;
+import io.adagate.database.subscribers.Subscriber;
+import io.adagate.database.triggers.AssetInsertedTrigger;
+import io.adagate.database.triggers.BlockInsertedTrigger;
+import io.adagate.exceptions.CardanoApiModuleException;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonObject;
 import io.vertx.pgclient.pubsub.PgSubscriber;
+import io.vertx.sqlclient.SqlConnection;
 
-import static io.adagate.verticles.database.DatabaseEventbusAddress.NEW_BLOCK;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
+import static io.vertx.core.CompositeFuture.all;
+import static java.lang.Math.min;
+import static java.lang.String.format;
+import static java.time.Duration.ofMinutes;
+import static java.util.stream.Collectors.toList;
+
+@SuppressWarnings("rawtypes")
 public class DatabaseSubscriberVerticle extends AbstractDatabaseVerticle {
     final static Logger LOGGER = LoggerFactory.getLogger(DatabaseSubscriberVerticle.class);
 
-    private PgSubscriber subscriber;
+    private long reconnectIntervalMs = 500;
+    private List<PgSubscriber> subscriptionHandlers = new ArrayList<>();
 
     @Override
     public void start(Promise<Void> startPromise) {
@@ -27,8 +42,16 @@ public class DatabaseSubscriberVerticle extends AbstractDatabaseVerticle {
                 .future()
                 .compose(this::createDatabaseTriggers)
                 .compose(this::setupSubscribers)
-                .onSuccess(startPromise::complete)
-                .onFailure(startPromise::fail);
+                .onSuccess(unused ->  startPromise.complete())
+                .onFailure(error -> {
+                    if (error instanceof Throwable) {
+                        LOGGER.error("Failed setting up trigger/ subscribers: ", (Throwable) error);
+                        startPromise.fail((Throwable) error);
+                    } else {
+                        LOGGER.error("Unknown Error - failed setting up trigger/ subscribers: " + error.toString());
+                        startPromise.fail(CardanoApiModuleException.INTERNAL_SERVER_500_ERROR);
+                    }
+                });
         } catch (Exception e) {
             LOGGER.error("Error ", e);
             throw e;
@@ -42,61 +65,65 @@ public class DatabaseSubscriberVerticle extends AbstractDatabaseVerticle {
             super.stop(superPromise);
 
             superPromise
-                    .future()
-                    .compose(r -> subscriber.close())
-                    .onSuccess(stopPromise::complete)
-                    .onFailure(stopPromise::fail);
+                .future()
+                .compose(r -> all(subscriptionHandlers.stream().map(PgSubscriber::close).collect(toList())))
+                .onSuccess(cf -> stopPromise.complete())
+                .onFailure(stopPromise::fail)
+                .onComplete(aR -> subscriptionHandlers.clear());
         } catch (Exception e) {
             LOGGER.error("Error ", e);
             throw e;
         }
     }
 
-    /* Private */
-
-    private Future<Void> createDatabaseTriggers(Void unused) {
-        return pool
-                .getConnection()
-                .compose(new BlockInsertedTrigger(pool));
+    @Override
+    protected boolean canBecomeIdle() {
+        return false;
     }
 
-    private Future<Void> setupSubscribers(Void unused) {
-        final Promise promise = Promise.promise();
-        subscriber = PgSubscriber.subscriber(vertx, connectionOps);
-        subscriber.connect(asyncResult -> {
-            if (asyncResult.failed()) {
-                LOGGER.error("Failed listening for new blocks", asyncResult.cause());
-                promise.fail(asyncResult.cause());
-                return;
-            }
-            subscriber
-                    .channel(BlockInsertedTrigger.NEW_BLOCK_CHANNEL)
-                    .handler(msg -> {
-                        JsonObject message = new JsonObject(msg);
-                        vertx
-                            .eventBus()
-                            .request(
-                                GetBlockByNumberOrHash.ADDRESS,
-                                new JsonObject()
-                                        .put("id", message.getInteger("id"))
-                                        .put("column", "id")
-                            )
-                            .onSuccess(blockMsg -> {
-                                vertx
-                                    .eventBus()
-                                    .send(
-                                        NEW_BLOCK.getAddress(),
-                                        Json.encode(
-                                            ((JsonObject) blockMsg.body())
-                                                .put("instanceId", INSTANCE_ID))
-                                    );
-                            })
-                            .onFailure(err -> LOGGER.error("Failed: " + err.getMessage(), err));
-                    })
-                    .exceptionHandler(err -> LOGGER.error("Channel Subscription Error: ", err));
+    /* Private */
 
-            promise.complete();
-        });
-        return promise.future();
+    private Future createDatabaseTriggers(Void unused) {
+        final List<Function<SqlConnection, Future<Void>>> triggers = Arrays.asList(
+            new BlockInsertedTrigger(pool),
+            new AssetInsertedTrigger(pool)
+        );
+        final List<Future> futures = new ArrayList<>();
+        for (Function<SqlConnection, Future<Void>> trigger: triggers) {
+            futures.add(pool.withConnection(trigger));
+        }
+        return all(futures);
+    }
+
+    private Future setupSubscribers(Object unused) {
+        final List<Future> futures = new ArrayList<>();
+        final List<Subscriber> subscribers = Arrays.asList(
+            new AssetInsertSubscriber(vertx),
+            new BlockInsertSubscriber(vertx)
+        );
+
+        for (Subscriber subscriber: subscribers) {
+            final PgSubscriber subHandler = PgSubscriber.subscriber(vertx, connectionOps);
+            futures.add(
+                subHandler
+                    .reconnectPolicy(retry -> {
+                        final long interval = min(reconnectIntervalMs * reconnectIntervalMs, ofMinutes(5).toMillis());
+                        LOGGER.warn(format("[%s] Subscription Disconnected: Retry in %d", getClass().getSimpleName(), interval));
+                        reconnectIntervalMs *= reconnectIntervalMs;
+                        return interval;
+                    })
+                    .connect()
+                    .map(v -> subHandler
+                                .channel(subscriber.getChannel())
+                                .handler(subscriber)
+                                .exceptionHandler(err -> {
+                                    LOGGER.error(String.format("[%s] Subscriber Error: ", subscriber.getChannel()), err);
+                                }))
+                    .onFailure(err -> LOGGER.error(format("[%s] Failed listening", subscriber.getChannel()), err.getCause()))
+            );
+            this.subscriptionHandlers.add(subHandler);
+        }
+
+        return all(futures);
     }
 }
