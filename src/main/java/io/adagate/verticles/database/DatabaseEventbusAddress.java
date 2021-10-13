e2b6c76f80ac8104319900893bf5c5091887fc28
William Wolff <william@cityxcape.com>
2021-10-13 21:07:48 +0200

    [DOC] Added documentation on subscribable eventbus addresses for clients

--- src/main/java/io/adagate/verticles/database/DatabaseEventbusAddress.java
+++ src/main/java/io/adagate/verticles/database/DatabaseEventbusAddress.java
@@ -3,11 +3,18 @@ package io.adagate.verticles.database;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * This type defines messages for certain database events
+ * which can be subscribed to from the client.
+ */
 @Getter
 @AllArgsConstructor
 public enum DatabaseEventbusAddress {
 
-    NEW_BLOCK("adagate.io.blocks.new");
+    /** Any new asset minted (inserted to database) will trigger this message. */
+    NEW_ASSET("io.adagate.asset.new"),
+    /** Any new block produced (inserted to database) will trigger this message. */
+    NEW_BLOCK("io.adagate.block.new");
 
     private String address;
 }
