dee2d6dbabe76a94195c2a9359a82b7a6ab11940
William Wolff <william@cityxcape.com>
2021-10-14 09:29:47 +0200

    [CLEAN] Removed unneeded configurations and ignored directories

--- src/main/java/io/adagate/ApiConstants.java
+++ src/main/java/io/adagate/ApiConstants.java
@@ -8,6 +8,5 @@ public final class ApiConstants {
     public static final int DEFAULT_POLICY_LENGTH = 56;
     public static final int MAX_QUERY_LIMIT = 100;
     public static final int DEFAULT_QUERY_OFFSET = 0;
-    public static final String APIKEY_HEADER = "adagate_id";
     public static final String DEFAULT_QUERY_ORDER = QueryOrder.ASC.name().toUpperCase();
 }
