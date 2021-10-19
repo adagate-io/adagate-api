97ded4ac502de5380b30c8549a4a3e5dbd041642
William Wolff <william@cityxcape.com>
2021-10-19 11:22:48 +0200

    [API] Added get epoch tests

--- src/test/java/io/adagate/handlers/routes/epochs/GetEpochTests.java
+++ src/test/java/io/adagate/handlers/routes/epochs/GetEpochTests.java
@@ -20,56 +20,57 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_ENCODING;
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 import static java.lang.String.format;
 
+@DisplayName("GET /epochs/<epoch_number>")
 public final class GetEpochTests extends AbstractApiTest {
 
     @Test
     @DisplayName("GET /epochs/<epoch_number>")
     void testGetEpochByNumber(Vertx vertx, VertxTestContext context, WebClient client) {
         testRequest(client, HttpMethod.GET, format("/epochs/%d", TEST_EPOCH_NUMBER))
-                .expect(
-                    statusCode(OK.code()),
-                    statusMessage(OK.reasonPhrase()),
-                    responseHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString()),
-                    responseHeader(CONTENT_ENCODING.toString(), GZIP),
-                    assertFieldEquals("epoch", TEST_EPOCH_NUMBER),
-                    assertNonNullField("start_time", Integer.class),
-                    assertNonNullField("end_time", Integer.class),
-                    assertFieldEquals("first_block_time", 1626731219),
-                    assertFieldEquals("last_block_time", 1627163081),
-                    assertFieldEquals("block_count", 21404),
-                    assertFieldEquals("tx_count", 131829),
-                    assertFieldEquals("output", "10662572699883986"),
-                    assertFieldEquals("fees", "27651228864"),
-                    assertFieldEquals("active_stake", "23210733595257321")
-                )
-                .send(context)
-                .onSuccess(response -> context.completeNow())
-                .onFailure(context::failNow);
+            .expect(
+                statusCode(OK.code()),
+                statusMessage(OK.reasonPhrase()),
+                responseHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString()),
+                responseHeader(CONTENT_ENCODING.toString(), GZIP),
+                assertFieldEquals("epoch", TEST_EPOCH_NUMBER),
+                assertNonNullField("start_time", Integer.class),
+                assertNonNullField("end_time", Integer.class),
+                assertFieldEquals("first_block_time", 1626731219),
+                assertFieldEquals("last_block_time", 1627163081),
+                assertFieldEquals("block_count", 21404),
+                assertFieldEquals("tx_count", 131829),
+                assertFieldEquals("output", "10662572699883986"),
+                assertFieldEquals("fees", "27651228864"),
+                assertFieldEquals("active_stake", "23210733595257321")
+            )
+            .send(context)
+            .onSuccess(response -> context.completeNow())
+            .onFailure(context::failNow);
     }
 
     @Test
     @DisplayName("GET /epochs/latest")
     void testGetLatestEpoch(Vertx vertx, VertxTestContext context, WebClient client) {
         testRequest(client, HttpMethod.GET, "/epochs/latest")
-                .expect(
-                    statusCode(OK.code()),
-                    statusMessage(OK.reasonPhrase()),
-                    responseHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString()),
-                    responseHeader(CONTENT_ENCODING.toString(), GZIP),
-                    assertNonNullField("epoch", Integer.class),
-                    assertNonNullField("start_time", Integer.class),
-                    assertNonNullField("end_time", Integer.class),
-                    assertNonNullField("first_block_time", Integer.class),
-                    assertNonNullField("last_block_time", Integer.class),
-                    assertNonNullField("block_count", Integer.class),
-                    assertNonNullField("tx_count", Integer.class),
-                    assertNonNullField("output", String.class),
-                    assertNonNullField("fees", String.class),
-                    assertNonNullField("active_stake", String.class)
-                )
-                .send(context)
-                .onSuccess(response -> context.completeNow())
-                .onFailure(context::failNow);
+            .expect(
+                statusCode(OK.code()),
+                statusMessage(OK.reasonPhrase()),
+                responseHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString()),
+                responseHeader(CONTENT_ENCODING.toString(), GZIP),
+                assertNonNullField("epoch", Integer.class),
+                assertNonNullField("start_time", Integer.class),
+                assertNonNullField("end_time", Integer.class),
+                assertNonNullField("first_block_time", Integer.class),
+                assertNonNullField("last_block_time", Integer.class),
+                assertNonNullField("block_count", Integer.class),
+                assertNonNullField("tx_count", Integer.class),
+                assertNonNullField("output", String.class),
+                assertNonNullField("fees", String.class),
+                assertNonNullField("active_stake", String.class)
+            )
+            .send(context)
+            .onSuccess(response -> context.completeNow())
+            .onFailure(context::failNow);
     }
 
     @Test
@@ -95,7 +96,7 @@ public final class GetEpochTests extends AbstractApiTest {
                     statusCode(BAD_REQUEST.code()),
                     statusMessage(BAD_REQUEST.reasonPhrase()),
                     responseHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString()),
-                    assertFieldEquals("message", "params.epochNumber should be integer")
+                    assertFieldEquals("message", "params.epochNumber should be a number")
                 )
                 .send(context)
                 .onSuccess(response -> context.completeNow())
