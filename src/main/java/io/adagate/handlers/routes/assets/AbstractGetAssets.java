ac0c4fa31d95a853f52a19eeb90428c883e8bcca
William Wolff <william@cityxcape.com>
2021-10-21 18:34:48 +0200

    [API] Extended property init for abstract type

--- src/main/java/io/adagate/handlers/routes/assets/AbstractGetAssets.java
+++ src/main/java/io/adagate/handlers/routes/assets/AbstractGetAssets.java
@@ -11,16 +11,17 @@ import io.vertx.ext.web.RoutingContext;
 import static io.adagate.ApiConstants.DEFAULT_QUERY_OFFSET;
 import static io.adagate.ApiConstants.MAX_QUERY_LIMIT;
 import static io.adagate.exceptions.AdaGateModuleException.BAD_REQUEST_400_ERROR;
+import static java.lang.Math.max;
 import static java.util.Objects.isNull;
 
-abstract class AbstractAssetAddresses extends AbstractRouteHandler {
+abstract class AbstractGetAssets extends AbstractRouteHandler {
 
     protected String assetId;
     protected int count;
     protected int page;
     protected String order;
 
-    AbstractAssetAddresses(Vertx vertx) { super(vertx); }
+    AbstractGetAssets(Vertx vertx) { super(vertx); }
 
     protected final String normalizeAssetId(final RoutingContext context) {
         final String rawAssetId = context.request().getParam("assetId");
@@ -53,13 +54,6 @@ abstract class AbstractAssetAddresses extends AbstractRouteHandler {
         }
 
         try {
-            page = getParameter(req.getParam("page"), Integer.class, DEFAULT_QUERY_OFFSET);
-        } catch (AdaGateModuleException e) {
-            handleError(BAD_REQUEST_400_ERROR, "querystring.page should be integer", context);
-            return;
-        }
-
-        try {
             count = getParameter(req.getParam("count"), Integer.class, MAX_QUERY_LIMIT);
             if (count <= 0) { count = MAX_QUERY_LIMIT; }
             if (count > MAX_QUERY_LIMIT) {
@@ -68,6 +62,14 @@ abstract class AbstractAssetAddresses extends AbstractRouteHandler {
             }
         } catch (AdaGateModuleException e) {
             handleError(BAD_REQUEST_400_ERROR, "querystring.count should be integer", context);
+            return;
+        }
+
+        try {
+            page = getParameter(req.getParam("page"), Integer.class, DEFAULT_QUERY_OFFSET);
+            page = max(0, page - 1) * count;
+        } catch (AdaGateModuleException e) {
+            handleError(BAD_REQUEST_400_ERROR, "querystring.page should be integer", context);
             return;
         }
     }
